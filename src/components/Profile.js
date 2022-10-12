f6325e1f527c009703e346bbe3bca558b24defe7
Lukas Gonzalez <lukegonz47@gmail.com>
2022-10-11 20:48:00 -0400

    added middlewares and logout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,12 @@
 
 function Profile(){
     return(
+    <div>
         <h1>welcome to ur profile</h1>
+        <form className="logout-button buttons" id="logout-form" action="/" method="POST">
+            <button className="button-34" role="button">Logout</button>
+        </form>
+    </div>
     )
 }
 
